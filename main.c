0e9c009962167d528093309a302b2f70c43cc2c6
KoonPorZa <mikuza45956@gmail.com>
2020-11-04 21:10:38 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+long SumNum(long n);
+int main(void) {
+  long n,sum;
+  printf("Enter number : ");
+  scanf("%ld",&n);
+  sum = SumNum(n);
+  printf("Summation of number : %ld\n",sum);
+  return 0;
+}
+long SumNum(long n)
+{
+  long sum=0;
+  while (n>0) {
+    sum = sum + (n%10); 
+    n /= 10; 
+  }
+  return sum;
+}
